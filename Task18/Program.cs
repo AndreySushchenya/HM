fd67508b36ed4f9dfb1d2d909168959e349cb0df
Andrey <sushenya2015@yandex.by>
2023-06-08 14:43:22 +0300

    Диапазон_заданной_четверти

---
+++ Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер четверти");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i == 1)
+{
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if (i == 2)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+else if (i == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+else if (i == 4)
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else 
+{
+    Console.WriteLine("Не является номером четверти");
+}
