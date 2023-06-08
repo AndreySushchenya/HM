e1935cd5490c4ab6d42f28a800a07b4bf3ae23a4
Andrey <sushenya2015@yandex.by>
2023-06-08 14:52:55 +0300

    Таблица_квадратов

---
+++ Square/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+int square = 0;
+
+while (start < (i + 1))
+{
+    square = start * start;
+    Console.WriteLine(square);
+    start++;
+}
