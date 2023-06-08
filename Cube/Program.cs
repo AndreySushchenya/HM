9406916c7c246c66e3ac263bdc7e472cd7eb9815
Andrey <sushenya2015@yandex.by>
2023-06-08 15:56:22 +0300

    Cube

---
+++ Cube/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+Double cube = 0;
+
+while (start < (i + 1))
+{
+    cube = Math.Pow(start,3);
+    Console.WriteLine(cube);
+    start++;
+}
