7540a69d992ee9b3b8c84fefbb9ee5968030a925
Andrey <sushenya2015@yandex.by>
2023-06-08 15:17:28 +0300

    Расстояние_между_точками_в_2D_пространстве

---
+++ Length_2D/Program.cs
@@ -0,0 +1,19 @@
+﻿
+            Double x1=0;
+            Double x2=0;
+            Double y1=0;
+            Double y2=0;
+            Double rasst;
+            Console.Write("x1=");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x2=");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1=");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2=");
+            y2 = Convert.ToDouble(Console.ReadLine());
+    
+                rasst = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2)); // Math.Sqrt - высчитывает квадратный корень выражения в скобках
+                Console.WriteLine("rasst:" + rasst);                      // Math.Pow  - возводит первое число в скобке в степень (второе число в скобке)
+                                                                          // MAth.Pow(4,3) - 4 возведет в 3 степень
+                                                                          
